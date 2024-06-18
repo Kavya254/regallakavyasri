dbc12920b3afbe38bc11d836f69d66b7e52d3490
Regalla Kavya Sri <regallakavyasri@Regallas-MacBook-Air.local>
2024-06-18 21:29:40 +0530

    First commit

---
+++ Desktop/Python Practice/Practice.py
@@ -0,0 +1,23 @@
+for X in range(10):
+    while(X < 5 & X >0):
+        print('TRUE')
+    if X<2:
+        print('K')
+        X-=1
+    else:
+        print('L')
+        break
+print('Concept of while Loop')
+if X > 9:
+    print('XYZ')
+else:
+    print('ABC')
+for Y in range(10):
+    if Y <= 2:
+        print('TRUE')
+        break
+    elif Y == 0:
+        print('FALSE')
+    else:
+        print('truefalse')
+        continue
